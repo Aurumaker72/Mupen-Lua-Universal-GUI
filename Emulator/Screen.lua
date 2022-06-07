668155faafadefe06ec7e4c8e1c34e3c2e51e128
Alex <48759429+Aurumaker72@users.noreply.github.com>
2022-06-07 14:12:37 +0200

    Fix upscaling

--- Emulator/Screen.lua
+++ Emulator/Screen.lua
@@ -1,31 +1,35 @@
 Screen = {
     IsExpanded = false,
     ExpandedOffset = 227,
+    DesignerWidth = 800,
+    DesignerHeight = 600,
     Dimensions = {
         InitialWidth = 0,
         InitialHeight = 0,
         Width = 0,
         Height = 0,
         ScalingX = 1,
-        ScalingY = 1,
+        ScalingY = 1
     }
 }
-
 
 function Screen.Dimensions.Update()
     d = wgui.info()
     Screen.Dimensions.Width = d.width
     Screen.Dimensions.Height = d.height
 
-    
     if Screen.Dimensions.InitialWidth == 0 or Screen.Dimensions.InitialHeight == 0 then
         Screen.Dimensions.InitialWidth = d.width
         Screen.Dimensions.InitialHeight = d.height
 
-        Screen.Dimensions.ScalingX = Screen.Dimensions.Width / 800
-        Screen.Dimensions.ScalingY = Screen.Dimensions.Height / 600
-        
+        if Screen.Dimensions.Width < Screen.DesignerWidth then
+            Screen.Dimensions.ScalingX = Screen.Dimensions.Width / Screen.DesignerWidth
         end
+        if Screen.Dimensions.Height < Screen.DesignerHeight then
+            Screen.Dimensions.ScalingY = Screen.Dimensions.Height / Screen.DesignerHeight
+        end
+
+    end
 end
 
 function Screen.Expand()
@@ -34,7 +38,7 @@ function Screen.Expand()
     if Screen.IsExpanded then
         return
     end
-    wgui.resize(math.tointeger (math.floor(Screen.Dimensions.Width + (Screen.ExpandedOffset))), Screen.Dimensions.Height)
+    wgui.resize(math.tointeger(math.floor(Screen.Dimensions.Width + (Screen.ExpandedOffset))), Screen.Dimensions.Height)
     Screen.Dimensions.Update()
     IsExpanded = true
 
